05f9934f46e1360e689c72aa24f4bbde1ea4b7bc
Sam Kronick <sam.kronick@mapbox.com>
2017-04-05 14:04:12 -0700

    Fixed module export definitions with browserify

--- dist/threebox.js
+++ dist/threebox.js
@@ -1,8 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = exports = {
-    Threebox: require('./src/Threebox'),
-    THREE: require('./src/three64.js')
-}
+window.Threebox = require('./src/Threebox'),
+window.THREE = require('./src/three64.js')
+
 },{"./src/Threebox":6,"./src/three64.js":10}],2:[function(require,module,exports){
 var THREE = require("../three64.js");
 var Threebox = require('../Threebox.js');
